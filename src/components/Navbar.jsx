b28e655fc1b7951990da6686748e308ef05c60a8
gniraj1432 <gniraj1432@gmail.com>
2025-03-19 16:42:16 +0530

    Mobile View CSS Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/styles/Navbar.css";
 
 const Navbar = () => {
-  
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="navbar">
@@ -11,14 +11,20 @@ const Navbar = () => {
         <Link to="/">SCSMS</Link>
       </div>
 
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/">Logout</Link></li>
-      </ul>
+      {/* Hamburger Menu Icon */}
+      <div className={`hamburger ${menuOpen ? "open" : ""}`} onClick={() => setMenuOpen(!menuOpen)}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
 
-      
+      {/* Navigation Links */}
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+        <li><Link to="/about" onClick={() => setMenuOpen(false)}>About Us</Link></li>
+        <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Logout</Link></li>
+      </ul>
     </nav>  
   );
 };
